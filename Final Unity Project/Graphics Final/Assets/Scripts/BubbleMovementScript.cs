c7cb749eabaca8358f1e8b76ab37703edbb33eff
Brandon Boras <brandon.boras@mymail.champlain.edu>
2021-05-01 13:05:21 -0400

    spruced up the animation a bit

--- Final Unity Project/Graphics Final/Assets/Scripts/BubbleMovementScript.cs
+++ Final Unity Project/Graphics Final/Assets/Scripts/BubbleMovementScript.cs
@@ -23,7 +23,7 @@ public class BubbleMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateBobHeight();
+        //UpdateBobHeight();
         UpdateRotation();
     }
 
