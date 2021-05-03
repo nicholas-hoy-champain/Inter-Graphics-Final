ed1c87035f15d510eba1950409e4e3bb33c476b5
Brandon Boras <brandon.boras@mymail.champlain.edu>
2021-05-02 21:30:04 -0400

    fixing the normal map

--- Final Unity Project/Graphics Final/Assets/Scripts/`ToDo.cs
+++ Final Unity Project/Graphics Final/Assets/Scripts/`ToDo.cs
@@ -3,6 +3,8 @@
 ///
 ///   Shader
 /// 
+/// 
+/// 
 /// 1 - noise effect is used to distort the uvs by a set amount with time to cause waves
 /// 2 - the spiking effect noticed towards the bottom-left of this bubble https://upload.wikimedia.org/wikipedia/commons/1/18/Soap_bubble_sky.jpg
 ///     maybe splicing two things based on a really sharp sin wave of the uv.y?
